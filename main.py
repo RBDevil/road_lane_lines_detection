69e1536d4ec6c87bf07dbae1c07ed08da4c9d9a1
RBDevil <verseghi.boldizsar@gmail.com>
2022-10-14 17:05:56 +0200

    Test pipeline with videos

--- main.py
+++ main.py
@@ -1,54 +1,15 @@
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 import cv2
-import numpy as np
+import image_processor
 
-image = cv2.imread('road.png')
-original_image = cv2.imread('road.png')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.GaussianBlur(image, (3, 3),cv2.BORDER_DEFAULT)
-image = cv2.Canny(image, 70, 140)
+video = cv2.VideoCapture('dashcam_video4.mp4')
 
-height = image.shape[0]
-width = image.shape[1]
+while(video.isOpened()):
+    ret, frame = video.read()
+    frame = image_processor.process_image(frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-region_of_interest_vertices = [
-    (0 + width/10, height), 
-    (width/2, height/2),
-    (width - width/10, height)
-    ]
-
-def region_of_interest(image, vertices) :
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, vertices, 255)
-    image = cv2.bitwise_and(image, mask)
-    return image
-
-def draw_lines(image, line_vectors):
-    image = np.copy(image)
-    lines_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
-
-    for line in line_vectors:
-        for x1, y1, x2, y2 in line:
-            cv2.line(lines_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=3)
-    
-    image = cv2.addWeighted(image, 0.8, lines_image, 1, 0.0)
-    return image
-
-image = region_of_interest(image, 
-np.array([region_of_interest_vertices], np.int32)
-)
-
-lines = cv2.HoughLinesP(
-    image,
-    rho=6,
-    theta=np.pi/60,
-    threshold=160,
-    lines=np.array([]),
-    minLineLength=40,
-    maxLineGap=25
-)
-
-image = draw_lines(original_image, lines)
-
-plt.imshow(image, cmap="gray", vmin=0, vmax=255)
-plt.show()
+video.release()
+cv2.destroyAllWindows()
