58213d32ab245850ba05f21f18ea2ddee5e49c85
RBDevil <verseghi.boldizsar@gmail.com>
2022-10-24 12:48:49 +0200

    Add previous frame check

--- image_processor.py
+++ image_processor.py
@@ -1,4 +1,3 @@
-from types import NoneType
 import cv2
 import numpy as np
 
@@ -13,7 +12,7 @@ def draw_lines(image, line_vectors):
 
     for line in line_vectors:
         for x1, y1, x2, y2 in line:
-            cv2.line(lines_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=3)
+            cv2.line(lines_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=5)
     
     image = cv2.addWeighted(image, 0.8, lines_image, 1, 0.0)
     return image
@@ -25,10 +24,10 @@ def sort_lines_by_side(line_vectors):
     for line in line_vectors:
         for x1, y1, x2, y2 in line:
             a, b = create_line(x1, y1, x2, y2)
-            if (a > 0):
+            if (a < 0):
                 left_lines.append([a, b])
             else:
-                if (a < 0):
+                if (a > 0):
                     right_lines.append([a, b])
 
     return left_lines, right_lines
@@ -63,7 +62,7 @@ def get_points_from_line(a, b, image):
         y1 = height
         x1 = (y1 - b) / a
 
-        y2 = height / 3 * 2
+        y2 = height / 20 * 13
         x2 = (y2 - b) / a
 
         return [round(x1), round(y1), round(x2), round(y2)]
@@ -71,9 +70,15 @@ def get_points_from_line(a, b, image):
     else:
         return [0,0,0,0]
 
+def create_line(x1, y1, x2, y2):
+    if (x2 != x1):
+        a = (y2 - y1) / (x2 - x1)
+        b = (x2 * y1 - x1 * y2) / (x2 - x1)
+        return a, b
+    else:
+        return 0, 0    
 
-
-def process_image(image):
+def process_image(image, previous_left_line, previous_right_line):
     original_image = np.copy(image)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.GaussianBlur(image, (3, 3),cv2.BORDER_DEFAULT)
@@ -102,14 +107,21 @@ def process_image(image):
         minLineLength=40,
         maxLineGap=25
     )
-
-    left_lines, right_lines = sort_lines_by_side(lines)
-    a, b = average_lines([left_lines])
-    left_line = get_points_from_line(a, b, image)
-    a, b = average_lines([right_lines])
-    right_line = get_points_from_line(a, b, image)
-
-
     
-    return draw_lines(original_image, [[left_line, right_line]])
-    #return draw_lines(original_image, lines)
+    left_lines, right_lines = sort_lines_by_side(lines)
+
+    a, b = average_lines([left_lines])
+    if (a != 0 and b != 0):
+        previous_left_line = [a, b]
+        left_line = get_points_from_line(a, b, image)
+    else:
+        left_line = get_points_from_line(previous_left_line[0], previous_left_line[1], image)
+    
+    a, b = average_lines([right_lines])
+    if (a != 0 and b != 0):
+        previous_right_line = [a, b]
+        right_line = get_points_from_line(a, b, image)
+    else:
+        right_line = get_points_from_line(previous_right_line[0], previous_right_line[1], image)
+    
+    return draw_lines(original_image, [[left_line, right_line]]), previous_left_line, previous_right_line
--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import matplotlib.pyplot as plt
 import cv2
 import image_processor
 
-video = cv2.VideoCapture('dashcam_video4.mp4')
+video = cv2.VideoCapture('dashcam_video3.mp4')
+
+previous_left_line = [0, 0]
+previous_right_line = [0, 0]
 
 while(video.isOpened()):
     ret, frame = video.read()
-    frame = image_processor.process_image(frame)
+    frame, previous_left_line, previous_right_line = image_processor.process_image(
+        frame, 
+        previous_left_line, 
+        previous_right_line
+        )
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
